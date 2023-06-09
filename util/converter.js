eae357539ffe0c76acc7049812131c478d86c4df
JanvElswijk <jajp.vanelswijk@student.avans.nl>
2023-06-09 13:35:56 +0200

    bruh 5

--- util/converter.js
+++ util/converter.js
@@ -1,6 +1,6 @@
 const dateConverter = (dateOfBirth) => {
     const dateArray = dateOfBirth.split('-');
-    if (dateArray[0].length === 4) {
+    if (dateArray[2].length === 4) {
         return `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
     } else {
         return dateOfBirth;
